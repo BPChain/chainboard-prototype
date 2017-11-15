79fec063d8cb8f82b52e34e94cdddf9f20318998
Tom Lichtenstein <tomli96@web.de>
2017-11-15 16:04:02 +0100

    Change DataRetrieval to refresh hash rate automatically

--- src/DataRetrieval.js
+++ src/DataRetrieval.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import axios from 'axios';
 
+
 class DataRetrieval extends React.Component {
 
-  componentDidMount() {
-    this.getHashRate();
-  }
-  
   constructor(props) {
-    super(props);
+    super(props)
+    this.timer = 0
     this.state = {
-      hashRate: []
+      hashRate: this.getHashRate()
     }
   }
- 
-  getHashRate = () => {
-    axios.get('https://api.nanopool.org/v1/eth/network/avgblocktime')
-    .then( (response) => {
-      this.setState({hashRate: response.data.data});
-    })
-    .catch( (error) => {
-      console.log(error);
-    });
+
+  componentWillMount() {
+    this.timer = setTimeout(() => {
+      this.getHashRate()
+      this.componentWillMount();
+    }, 5000)
   }
 
-  render() {    
+  getHashRate = () => {
+    axios
+      .get('https://api.nanopool.org/v1/eth/network/avgblocktime')
+      .then( (response) => {
+        this.setState({hashRate: response.data.data})
+      })
+      .catch( (error) => {
+        console.log(error)
+      })
+  }
+
+  render() {
     return (
       <div>
         <p>Hashrate is {this.state.hashRate}</p>
