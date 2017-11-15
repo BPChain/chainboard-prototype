9d0efcad5fcefaa446f1a2e4fbfb284bef8138ef
Simon Siegert <sisiegert@uni-potsdam.de>
2017-11-15 16:12:50 +0100

    Include semantic ui

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className="ui container">
         <header className="App-header">
 
           <h1 className="App-title">Welcome to ChainBoard</h1>
