ea36dc1a458c4fb9e1f9af5e6b549e743b8fb109
Tom Lichtenstein <tomli96@web.de>
2017-11-21 17:47:41 +0100

    Change datasource to our own API-server

--- src/DataRetrieval.js
+++ src/DataRetrieval.js
@@ -8,47 +8,49 @@ class DataRetrieval extends React.Component {
     super(props)
     this.timer = 0
     this.state = {
-      blockTime: this.getBlockTime(),
-      timeToEpoch: this.getTimeToNextEpoch(),
+      activeMiners: 0,
+      activeWorkers: 0,
+      averageBlockTime: 0, 
+      blockTimeDifficulty: 0,
+      hashRate: 0,
+      timeToNextEpoch: 0,
     }
   }
 
   componentWillMount () {
     this.timer = setTimeout(() => {
-      this.getBlockTime()
-      this.getTimeToNextEpoch()
+      this.getEthereumPublicData()
       this.componentWillMount()
     }, 5000)
   }
 
-  getBlockTime = () => {
+  getEthereumPublicData = () => {
     axios
-      .get('https://api.nanopool.org/v1/eth/network/avgblocktime')
-      .then( (response) => {
-        this.setState({blockTime: response.data.data})
+      .get('http://localhost:2020/api/ethereum/publicStat')
+      .then((response) => {
+        const data = response.data
+        console.info(data.activeMiners)
+        this.setState({
+          activeMiners: data.activeMiners,
+          activeWorkers: data.activeWorkers,
+          averageBlockTime: data.averageBlockTime, 
+          blockTimeDifficulty: data.blockTimeDifficulty,
+          hashRate: data.hashRate,
+          timeToNextEpoch: data.timeToNextEpoch,
+        })
       })
       .catch( (error) => {
         console.log(error)
       })
   }
 
-  getTimeToNextEpoch = () => {
-    axios
-      .get('https://api.nanopool.org/v1/eth/network/timetonextepoch')
-      .then( (response) => {
-        this.setState({timeToEpoch: response.data.data})
-      })
-      .catch( (error) => {
-        console.log(error)
-      })
-  }
 
   render () {
     return (
       <div className="ui container">
         <br></br>
-        <p>Average Block Time is {this.state.blockTime} seconds.</p>
-        <p>Time to next Epoch is {this.state.timeToEpoch} seconds.</p>
+        <p>Average Block Time is {this.state.averageBlockTime} seconds.</p>
+        <p>Time to next Epoch is {this.state.timeToNextEpoch} seconds.</p>
       </div>
     )
   }
