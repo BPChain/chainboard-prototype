4ca8842f3936818b614f76db6562864d4f98dc07
Simon Siegert <sisiegert@uni-potsdam.de>
2017-11-21 15:45:34 +0100

    Move DataRetrival to App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'semantic-ui-css/semantic.min.css'
 import App from './App'
-import DataRetrieval from './DataRetrieval'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-ReactDOM.render(<DataRetrieval />, document.getElementById('nanopool'))
